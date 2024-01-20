92da5f406d6027fc340f7efb56f7fddc996a80bb
lorenz aichner <aichnerl@yahoo.de>
2024-01-20 15:08:55 +0100

    feat: remove nan

--- VAR.py
+++ VAR.py
@@ -2,15 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.vector_ar.var_model import VAR
+from statsmodels.tsa.stattools import grangercausalitytests, adfuller
+
+
+TARGET_VARIABLE = "Zone 1 Power Consumption"
+N_PERIOD_TO_PREDICT = 35*140
 
 def check_stationarity(series, threshold=0.05):
-    result = adfuller(series.dropna())
+    result = adfuller(series)
     if result[1] < threshold:
         return True
     else:
         return False
+
 
 # Load your dataset
 data = pd.read_csv("Tetuan/Tetuan_City_power_consumption.csv", delimiter=',')
@@ -33,52 +38,57 @@ for col in columns_to_plot:
     data[col] = data[col] + (-min_value + 1) if min_value <= 0 else data[col]
 
     # Apply log transformation
-    data[col + ' Log'] = np.log(data[col])
-
+    data[col] = np.log(data[col])
     # Apply differencing
-    data[col + ' Diff'] = data[col + ' Log'].diff(seasonality)
+    data[col] = data[col].diff(seasonality)
 
     # Apply rolling mean
-    data[col + ' Rolling Avg'] = data[col + ' Diff'].rolling(window=143).mean()
+    data[col] = data[col].rolling(window=143).mean()
 
     # Drop NaN values
-    data_to_plot = data[col + ' Rolling Avg'].dropna()
+    data_to_plot = data[col].dropna()
 
     # Plotting
-    data_to_plot.plot()
-    plt.savefig(f'dataplots/{col}_rolling_avg.png')
-    plt.close()
+    #data_to_plot.plot()
+    #plt.savefig(f'dataplots/{col}_rolling_avg.png')
+    #plt.close()
+
 
 
 # Path to the directory where plots are saved
 print("Plots saved in the 'dataplots' directory.")
+data.replace('', np.nan, inplace=True)
+data.dropna(inplace=True)
+data.to_csv("stationaryDataset.csv", index=False)
+
 
 #APPLY VECTOR AUTO REGRESSION
 cols = data.columns
 train = data[:int(0.9*(len(data)))]
 valid = data[int(0.9*(len(data))):]
 
-# Check for stationarity
-for col in columns_to_plot:
-    if not check_stationarity(data[col + ' Rolling Avg']):
-        print(f"Series {col} is not stationary")
-        exit(4)
+#Check for stationarity
+#for col in columns_to_plot:
+#    print({col})
+#    if not check_stationarity(data[col]):
+#        print(f"Series {col} is not stationary")
+#        exit(4)
 
 # Adjusting the VAR model fitting
+'''
 model = VAR(train)
-for i in [1,2,3,4,5,6,7,8,9]:
+for i in [1,2,3,4,5,6,7,8,9,10,20,30]:
     result = model.fit(i)
     print('Lag Order =', i)
     print('AIC : ', result.aic)
     print('BIC : ', result.bic)
     print('FPE : ', result.fpe)
     print('HQIC: ', result.hqic, '\n')
-
 '''
+grangers_test(data, 9)
 try:
     model = VAR(endog=train)
-    model_fit = model.fit(maxlags=5)  # You can adjust the number of lags
+    model_fit = model.fit(maxlags=9)  # You can adjust the number of lags
     # Rest of your VAR code
 except np.linalg.LinAlgError:
     print("SVD did not converge. Adjust the model or data preprocessing.")
-'''
